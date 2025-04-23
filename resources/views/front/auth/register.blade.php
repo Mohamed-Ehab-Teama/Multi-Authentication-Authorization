6fba6afd08f219f29672ebed305aef387c386e10
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-04-23 18:14:01 +0200

    Customize Front Registeraion

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -8,6 +8,7 @@
     <div class="container-xxl">
         <div class="authentication-wrapper authentication-basic container-p-y">
             <div class="authentication-inner">
+
                 <!-- Register Card -->
                 <div class="card">
                     <div class="card-body">
@@ -68,17 +69,27 @@
                         <h4 class="mb-2">Adventure starts here 🚀</h4>
                         <p class="mb-4">Make your app management easy and fun!</p>
 
-                        <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                        {{-- Registration Form --}}
+                        <form id="formAuthentication" class="mb-3" action="{{ route('register') }}" method="POST">
+                            @csrf
+
+                            {{-- Name --}}
                             <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username"
-                                    placeholder="Enter your username" autofocus />
+                                <label for="username" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="username" name="name"
+                                    :value="old('name')" placeholder="Enter your username" autofocus />
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
+
+                            {{-- Email --}}
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="email" name="email"
+                                <input type="text" class="form-control" id="email" name="email" :value="old('email')"
                                     placeholder="Enter your email" />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
+
+                            {{-- Password --}}
                             <div class="mb-3 form-password-toggle">
                                 <label class="form-label" for="password">Password</label>
                                 <div class="input-group input-group-merge">
@@ -87,23 +98,17 @@
                                         aria-describedby="password" />
                                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                 </div>
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
 
-                            <div class="mb-3">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
-                                    <label class="form-check-label" for="terms-conditions">
-                                        I agree to
-                                        <a href="javascript:void(0);">privacy policy & terms</a>
-                                    </label>
-                                </div>
-                            </div>
-                            <button class="btn btn-primary d-grid w-100">Sign up</button>
+
+                            <button type="submit" class="btn btn-primary d-grid w-100">Sign up</button>
                         </form>
+                        {{-- Registration Form End --}}
 
                         <p class="text-center">
                             <span>Already have an account?</span>
-                            <a href="auth-login-basic.html">
+                            <a href="{{ route('front.login') }}">
                                 <span>Sign in instead</span>
                             </a>
                         </p>
--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 // Front Routes
 Route::prefix('front')->name('front.')->group(function () {
-    Route::get('/', [FrontController::class, 'home'])->name('front.home');
-    Route::view('/login', 'front.auth.login');
-    Route::view('/register', 'front.auth.register');
-    Route::view('/forgetPassword', 'front.auth.forgetPassword');
+    Route::get('/', [FrontController::class, 'home'])->name('home')->middleware('auth');
+    Route::view('/login', 'front.auth.login')->name('login');
+    Route::view('/register', 'front.auth.register')->name('register');
+    Route::view('/forgetPassword', 'front.auth.forgetPassword')->name('forgetPassword');
 });
 
+require __DIR__.'/auth.php';
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-require __DIR__.'/auth.php';
